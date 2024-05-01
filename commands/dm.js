703d9740ee83e362613479d76b0a26cb8007e19b
BrqkenCode <valentin.martin.do@gmail.com>
2024-05-01 21:21:38 +0200

    Added DM User

---
+++ commands/dm.js
@@ -0,0 +1,58 @@
+const {  Permissions, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  data: {
+    name: 'dm',
+    description: 'Send a private message to a user via the bot',
+    options: [
+      {
+        name: 'user',
+        description: 'User',
+        type: 6,
+        required: true,
+      },
+      {
+        name: 'message',
+        description: 'The messge that you want to send',
+        type: 3,
+        required: true,
+      },
+
+    ],
+  },
+  async execute(interaction) {
+    const { member, options } = interaction;
+    const userOption = options.getUser('user'); 
+    const message = options.getString('message');
+    const user = userOption;
+    
+    
+    
+
+    const embed = new EmbedBuilder() 
+      .setColor('#0000ff')
+      .setTitle('Message from the Staff')
+      .setDescription("Please read the message carefully")
+      .addFields(
+        { name: 'Staff Member', value: `<@${member.user.id}>` },
+        {name: 'Message', value:  message}
+      )
+      .setTimestamp();
+
+    const requiredRole = member.roles.cache.some(role => role.name === 'Admin');
+    if (!requiredRole) {
+      return interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+    }
+
+    if (user) {
+      try {
+        await user.send({ embeds: [embed] });
+        await interaction.reply({ content: 'Message sent to the user successfully.', ephemeral: true });
+      } catch (error) {
+        console.error('Failed to send a message to the user:', error);
+      }
+    } else {
+      interaction.reply({ content: 'This user does not exist!' });
+    }
+  },
+};
