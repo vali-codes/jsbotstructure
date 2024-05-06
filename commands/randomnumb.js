cdfe1e7b1889fa8bbb8f8206bcb6d33869b7254d
BrqkenCode <valentin.martin.05@icloud.com>
2024-05-06 13:42:01 +0200

    Added randomnumber command

---
+++ commands/randomnumb.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const Discord = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('rn')
+    .setDescription('Generate a random number between 1 and 1000.'),
+  async execute(interaction) {
+    const number = Math.floor(Math.random() * 1000) + 1;
+    const embed = new EmbedBuilder()
+      .setColor(0x0099FF)
+      .setTitle('Number')
+      .setDescription(String(number))
+      .setTimestamp()
+    
+      
+    interaction.deleteReply();
+    interaction.channel.send({ embeds: [embed]});
+    interaction.deferReply();
+  },
+};
